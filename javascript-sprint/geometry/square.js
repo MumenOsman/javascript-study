7fc239934e3e8659f9f8f124c777ca2a5e1ab27a
Mumen <110625712+MumenOsman@users.noreply.github.com>
2026-06-23 19:52:38 +0300

    Add/update javascript-sprint\geometry\square.js

---
+++ javascript-sprint/geometry/square.js
@@ -0,0 +1,15 @@
+import { Rectangle } from './rectangle.js';
+
+export class Square extends Rectangle {
+    constructor(side) {
+        super(side, side);
+        this.side = side;
+    }
+}
+
+export const rectangle = new Rectangle(5, 3);
+export const square = new Square(4);
+
+
+
+
