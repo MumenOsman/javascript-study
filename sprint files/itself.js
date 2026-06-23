921cccc90ece4a453498cc6b9e3d6f1b944d4429
Mumen <110625712+MumenOsman@users.noreply.github.com>
2026-06-23 19:59:07 +0300

    Add sprint files/itself.js

---
+++ sprint files/itself.js
@@ -0,0 +1,15 @@
+const sender = {
+    name: "Tom", 
+    address: "123 Main St",
+}; 
+
+const deliveryPackage = {
+    contents: "porridge", 
+    sender: sender, 
+};
+
+const trackingInfo = {
+    sender: sender, 
+    status: "in transit"   
+}
+
