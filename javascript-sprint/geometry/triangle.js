e968580a3b5fa9c5ebb4ade707c03818734ae14a
Mumen <110625712+MumenOsman@users.noreply.github.com>
2026-06-23 19:52:41 +0300

    Add/update javascript-sprint\geometry\triangle.js

---
+++ javascript-sprint/geometry/triangle.js
@@ -0,0 +1,17 @@
+export class Triangle {
+    constructor(sideA, sideB, sideC) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+    area() {
+        const s = (this.sideA + this.sideB + this.sideC) / 2;
+        return Math.sqrt(s * (s - this.sideA) * (s - this.sideB) * (s - this.sideC));
+    }
+    perimeter() {
+        return this.sideA + this.sideB + this.sideC;
+    }
+}
+
+export const triangle = new Triangle(3, 4, 5);
+
