2672507076037fea7f1d6211d2a503e9f88441b0
Mumen <110625712+MumenOsman@users.noreply.github.com>
2026-06-23 19:59:18 +0300

    Add sprint files/maths.js

---
+++ sprint files/maths.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+    return a + b;
+} 
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+const a = 1; 
+const b = 3; 
+const aPlusB = add(a, b);
+
+console.log(aPlusB);
