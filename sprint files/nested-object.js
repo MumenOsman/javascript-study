c0d6601d1504ded8bc4756193ea33537dd14e241
Mumen <110625712+MumenOsman@users.noreply.github.com>
2026-06-23 19:59:23 +0300

    Add sprint files/nested-object.js

---
+++ sprint files/nested-object.js
@@ -0,0 +1,9 @@
+const userProfile = {
+    skills: ["JavaScript", "HTML", "CSS"],
+    person: {
+        isMarried: false,
+        age: 25,
+        middleName: undefined,
+        firstName: "Mumen",
+    }
+}
